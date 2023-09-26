8139e7beaaa0984fffbc70f6b04492a16351eb5b
Fox <lukito@ya.ru>
2023-09-26 20:30:09 +0300

    add test

---
+++ src/test/java/GitHubSoftAssertionsTests.java
@@ -0,0 +1,55 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class GitHubSoftAssertionsTests {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.baseUrl = "https://github.com";
+        Configuration.browserSize = "1920x1080";
+    }
+    @Test
+    void fillFormTest() {
+        open("/selenide/selenide");
+        $("#wiki-tab").click();
+        $("#wiki-pages-filter").setValue("SoftAssertions").pressEnter();
+        $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
+        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
+        $(".markdown-body").shouldHave(text("Using JUnit5 extend test class:"));
+        $(".markdown-body").shouldHave(text(
+                "@ExtendWith({SoftAssertsExtension.class})\n" +
+                        "class Tests {\n" +
+                        "  @Test\n" +
+                        "  void test() {\n" +
+                        "    Configuration.assertionMode = SOFT;\n" +
+                        "    open(\"page.html\");\n" +
+                        "\n" +
+                        "    $(\"#first\").should(visible).click();\n" +
+                        "    $(\"#second\").should(visible).click();\n" +
+                        "  }\n" +
+                        "}"
+        ));
+        $(".markdown-body").shouldHave(text("Or register extension inside test class:"));
+        $(".markdown-body").shouldHave(text(
+                "class Tests {\n" +
+                        "  @RegisterExtension \n" +
+                        "  static SoftAssertsExtension softAsserts = new SoftAssertsExtension();\n" +
+                        "\n" +
+                        "  @Test\n" +
+                        "  void test() {\n" +
+                        "    Configuration.assertionMode = SOFT;\n" +
+                        "    open(\"page.html\");\n" +
+                        "\n" +
+                        "    $(\"#first\").should(visible).click();\n" +
+                        "    $(\"#second\").should(visible).click();\n" +
+                        "  }\n" +
+                        "}"
+        ));
+    }
+
+}
